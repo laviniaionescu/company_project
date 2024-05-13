4a311c2573a2bac5c23256e1e9fb3cf7e3d0a4b5
LaviniaIonescu <laviniaionescu9933@gmail.com>
2024-05-13 18:54:00 +0300

    changed password from visible to base 64 encoded

--- main.py
+++ main.py
@@ -1,8 +1,11 @@
 import json
 import psycopg2 as ps
+import base64 as b64
 
 with open("config.json", "r") as f:
     config = json.loads(f.read())
+    config['password'] = b64.b64decode(config['password']).decode()
+
 
 with ps.connect(**config) as conn:
     with conn.cursor() as cursor:
